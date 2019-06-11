d326db17b95a587a9ffe46af11717d1b0cc607b1
Sailesh Dahal <saileshbro@gmail.com>
2019-06-11 10:00:48 +0545

    addes cros

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 require("dotenv").config();
+const cors = require("cors");
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 const port = process.env.PORT || 3000;
@@ -10,4 +12,6 @@ const userRouter = require("./routes/user");
 const diseaseRouter = require("./routes/disease");
 app.use("/api", userRouter);
 app.use("/api", diseaseRouter);
-app.listen(port, console.log(`Server listening at port ${port}`));
+app.listen(port, () => {
+  console.log(`Server listening at port ${port}`);
+});
