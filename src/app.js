edbbd31f39e313cb5ffc169c2d652ee8610b7d0a
Sailesh Dahal <saileshbro@gmail.com>
2019-06-21 11:34:36 +0545

    added route profile

--- src/app.js
+++ src/app.js
@@ -5,15 +5,19 @@ const bodyParser = require("body-parser");
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 const port = process.env.PORT || 3000;
 
 const userRouter = require("./routes/user");
 const diseaseRouter = require("./routes/disease");
 const postRouter = require("./routes/post");
+const profileRouter = require("./routes/profile");
 app.use("/api", userRouter);
 app.use("/api", diseaseRouter);
 app.use("/api", postRouter);
+app.use('/api', profileRouter);
 app.listen(port, () => {
   console.log(`Server listening at port ${port}`);
 });
