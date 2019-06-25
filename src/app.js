ea558f502d38612f693207661dcb0c198783d221
Sailesh Dahal <saileshbro@gmail.com>
2019-06-25 10:58:34 +0545

    added some routes

--- src/app.js
+++ src/app.js
@@ -12,12 +12,16 @@ const port = process.env.PORT || 3000;
 
 const userRouter = require("./routes/user");
 const diseaseRouter = require("./routes/disease");
+const symptomRouter = require("./routes/symptom");
 const postRouter = require("./routes/post");
 const profileRouter = require("./routes/profile");
+const tipsRouter = require("./routes/tips");
 app.use("/api", userRouter);
 app.use("/api", diseaseRouter);
 app.use("/api", postRouter);
 app.use('/api', profileRouter);
+app.use('/api', symptomRouter);
+app.use('/api', tipsRouter);
 app.listen(port, () => {
   console.log(`Server listening at port ${port}`);
 });
