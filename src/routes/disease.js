88396e0d2364325abfaceb107d38b7aa352e7568
Sailesh Dahal <saileshbro@gmail.com>
2019-06-25 22:14:14 +0545

    added route for top disease

--- src/routes/disease.js
+++ src/routes/disease.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const auth = require("../auth/auth");
 const diseaseController = require("../controller/diseaseController");
 router.get("/diseases", auth, diseaseController.getDiseases);
+router.get("/diseases/top", auth, diseaseController.topDiseases);
 router.get("/disease/:disease_id", auth, diseaseController.getDisease);
 router.post("/diseases", auth, diseaseController.addDiseases);
 router.patch("/disease/:disease_id", auth, diseaseController.updateDisease);
--- src/routes/symptom.js
+++ src/routes/symptom.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const auth = require("../auth/auth");
 const symptomController = require("../controller/symptomController");
 router.get("/symptoms", auth, symptomController.getSymptoms);
+router.get("/symptoms/top", auth, symptomController.topSymptoms);
 router.get("/symptom/:symptom_id", auth, symptomController.getSymptom);
 router.post("/symptoms", auth, symptomController.addSymptoms);
 router.patch("/symptom/:symptom_id", auth, symptomController.updateSymptom);
