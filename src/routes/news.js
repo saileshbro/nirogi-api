496fc2ceb8acb786d5de8ed0743c2bf8b4f25bf3
Sailesh Dahal <saileshbro@gmail.com>
2019-07-01 15:16:39 +0545

    updated news controller

--- src/routes/news.js
+++ src/routes/news.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const auth = require("../auth/auth");
-const newsController = require('../controller/newsController');
-router.get('/news', newsController.getAllNews);
+const newsController = require("../controller/newsController");
+router.get("/news", newsController.getAllNews);
+router.get("/news/:news_id", newsController.getNewsItem);
 router.post("/news", newsController.addNews);
 module.exports = router;
