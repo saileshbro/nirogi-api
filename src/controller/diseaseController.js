44b1cd34404d7134178c3faad1fd3f795f8ae6b7
Sailesh Dahal <saileshbro@gmail.com>
2019-07-07 14:07:58 +0545

    .

--- src/controller/diseaseController.js
+++ src/controller/diseaseController.js
@@ -1,7 +1,29 @@
 const pool = require("../database/database");
 module.exports.getDiseases = async (req, res) => {
   try {
-    const results = await pool.query("SELECT disease_id,disease,imageUrl,description FROM diseases ORDER BY disease ASC");
+    const results = await pool.query(
+      "SELECT disease_id,disease,imageUrl,description FROM diseases ORDER BY disease ASC"
+    );
+    if (results.length === 0) {
+      return res.status(404).json({
+        error: "Diseases not found"
+      });
+    }
+    return res.json({
+      diseases: results
+    });
+  } catch (error) {
+    return res.status(500).json({
+      error: "Internal server error"
+    });
+  }
+};
+module.exports.getSearchDiseases = async (req, res) => {
+  const query = req.params.search;
+  try {
+    const results = await pool.query(
+      `SELECT disease_id,disease,imageUrl,description FROM diseases WHERE disease LIKE "${query}%" ORDER BY disease ASC`
+    );
     if (results.length === 0) {
       return res.status(404).json({
         error: "Diseases not found"
@@ -19,7 +41,10 @@ module.exports.getDiseases = async (req, res) => {
 module.exports.getDisease = async (req, res) => {
   const disease_id = req.params.disease_id;
   try {
-    const results = await pool.query("SELECT disease_id,disease,imageUrl,body FROM diseases WHERE disease_id=?", [disease_id]);
+    const results = await pool.query(
+      "SELECT disease_id,disease,imageUrl,body FROM diseases WHERE disease_id=?",
+      [disease_id]
+    );
     if (results.length === 0) {
       return res.status(404).json({
         error: "Diseases not found"
@@ -38,13 +63,18 @@ module.exports.addDiseases = async (req, res) => {
   const imageUrl = req.body.imageUrl;
   const body = req.body.body;
   try {
-    const count = await pool.query("SELECT * FROM diseases WHERE disease=?", [disease]);
+    const count = await pool.query("SELECT * FROM diseases WHERE disease=?", [
+      disease
+    ]);
     if (count.length != 0) {
       return res.status(403).json({
         error: "Disease already exists."
       });
     } else {
-      const insert = await pool.query("INSERT INTO diseases SET disease=?,body=?,imageUrl=?,description=?", [disease, body, imageUrl, description]);
+      const insert = await pool.query(
+        "INSERT INTO diseases SET disease=?,body=?,imageUrl=?,description=?",
+        [disease, body, imageUrl, description]
+      );
       if (insert.affectedRows == 1) {
         return res.send({
           message: "Sucessfully inserted",
@@ -61,14 +91,19 @@ module.exports.addDiseases = async (req, res) => {
 module.exports.updateDisease = async (req, res) => {
   const disease_id = req.params.disease_id;
   const body = req.body.body;
-  const result = await pool.query("UPDATE diseases SET body=? WHERE disease_id=?", [body, disease_id]);
+  const result = await pool.query(
+    "UPDATE diseases SET body=? WHERE disease_id=?",
+    [body, disease_id]
+  );
   if (result) {
     return res.sendStatus(200);
   }
 };
 module.exports.topDiseases = async (req, res) => {
   try {
-    const results = await pool.query("SELECT disease_id,disease,imageUrl,description FROM diseases ORDER BY CHAR_LENGTH(body) DESC LIMIT 8");
+    const results = await pool.query(
+      "SELECT disease_id,disease,imageUrl,description FROM diseases ORDER BY CHAR_LENGTH(body) DESC LIMIT 8"
+    );
     if (results.length === 0) {
       return res.status(404).json({
         error: "Diseases not found"
--- src/routes/disease.js
+++ src/routes/disease.js
@@ -2,6 +2,11 @@ const router = require("express").Router();
 const auth = require("../auth/auth");
 const diseaseController = require("../controller/diseaseController");
 router.get("/diseases", auth, diseaseController.getDiseases);
+router.get(
+  "/diseases/search/:search",
+  auth,
+  diseaseController.getSearchDiseases
+);
 router.get("/diseases/top", auth, diseaseController.topDiseases);
 router.get("/disease/:disease_id", auth, diseaseController.getDisease);
 router.post("/diseases", auth, diseaseController.addDiseases);
